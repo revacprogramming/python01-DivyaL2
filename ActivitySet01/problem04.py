52be86ba38322bd1f91a4ef5379094bc2bb66a82
DivyaL2 <96220816+DivyaL2@users.noreply.github.com>
2022-05-14 14:41:50 +0000

    program4

--- ActivitySet01/problem04.py
+++ ActivitySet01/problem04.py
@@ -1,3 +1,10 @@
 # Conditional Execution
 
 hrs = input("Enter hours? ")
+h=float(hrs)
+rate=float(input("Enter the rate per hour? "))
+if h>40:
+  rate1=(rate*1.5)*(h-40)
+pay=((h-5)*rate)+rate1
+print(pay)
+
